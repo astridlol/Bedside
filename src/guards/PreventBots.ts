fbba9d51adc8d59708e822922ddd85bfc881edd2
astridlol <sites@astrid.sh>
2023-11-10 17:03:40 -0500

    fix(PreventBots): properly prevent bots

--- src/guards/PreventBots.ts
+++ src/guards/PreventBots.ts
@@ -1,9 +1,5 @@
 import { GuardFunction, ArgsOf } from 'discordx';
 
-export const PreventBots: GuardFunction<ArgsOf<'messageCreate'>> = async (
-	[message],
-	client,
-	next
-) => {
-	if (client.user.id !== message.author.id) await next();
+export const PreventBots: GuardFunction<ArgsOf<'messageCreate'>> = async ([message], _, next) => {
+	if (!message.author.bot) await next();
 };
